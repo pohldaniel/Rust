1cc1e830f1023a5406395933042b360008002185
Daniel Pohl <pohl.daniel@freenet.de>
2025-04-27 13:21:32 +0200

    rename project

--- 02WebGPU/src/app/interceptor/session.interceptor.ts
+++
@@ -1,8 +0,0 @@
-import {HttpHandlerFn, HttpInterceptorFn, HttpRequest, HttpResponse,} from '@angular/common/http';
-import {catchError, throwError} from 'rxjs';
-
-export const intercept: HttpInterceptorFn = (request : HttpRequest<unknown>, next : HttpHandlerFn) => {
-  return next(request).pipe(catchError(err => {     
-    return throwError(() => err);
-  })); 
-};
